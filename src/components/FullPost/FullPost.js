fc5be51a0e9c42a1c7981e9693b6f0c76f621650
ciamac-da <ciamac.davoudi@gmail.com>
2021-04-08 15:05:46 +0200

    Verified by Marti4n v1.0.10 Setting default Global Configs for Axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -11,7 +11,7 @@ class FullPost extends Component {
     if (this.props.id) {
       if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
         axios
-          .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+          .get("/posts/" + this.props.id)
           .then((response) => {
             this.setState({
               loadedPost: response.data,
@@ -27,7 +27,7 @@ class FullPost extends Component {
 
   deleteDataHandler = () => {
     axios
-    .delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+    .delete("/posts/" + this.props.id)
     .then(response =>{
         console.log(response)
     })
